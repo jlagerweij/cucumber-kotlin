7c05d3e7873f4449acd86a2848bfdec1ef467d47
Jos Lagerweij <jos@lagerwey.net>
2020-04-14 14:27:39 +0200

    Use Intellij IDEA 2020.1 version and upgrade Gherkin plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     base
-    kotlin("jvm") version "1.3.61"
-    id("org.jetbrains.intellij") version "0.4.15"
+    kotlin("jvm") version "1.3.71"
+    id("org.jetbrains.intellij") version "0.4.18"
 }
-val ideaVersion = extra.properties["ideaVersion"] as? String ?: "2019.3.2"
+val ideaVersion = extra.properties["ideaVersion"] as? String ?: "2020.1"
 val jetbrainsPublishToken: String by project
 
 apply {
@@ -22,32 +22,15 @@ intellij {
     instrumentCode = true
 
     when (ideaVersion) {
-        "2018.3" ->
-            setPlugins(
-                    "gherkin:183.4284.148",
-                    "org.jetbrains.kotlin:1.3.31-release-IJ2018.3-1"
-            )
-        "2019.1.3" ->
-            setPlugins(
-                    "gherkin:191.6707.7",
-                    "org.jetbrains.kotlin:1.3.31-release-IJ2019.1-1"
-            )
-        "2019.3.2" ->
+        // Gherkin plugin version: https://plugins.jetbrains.com/plugin/9164-gherkin/versions
+        "2020.1" ->
             setPlugins(
                     "java",
-                    "gherkin:193.6015.9",
-                    "org.jetbrains.kotlin:1.3.61-release-IJ2019.3-1"
+                    "gherkin:201.6668.60",
+                    "Kotlin"
             )
     }
 }
-
-//inline operator fun <T : Task> T.invoke(a: T.() -> Unit): T = apply(a)
-//val publishPlugin: PublishTask by tasks
-//publishPlugin {
-//    username(jetbrainsPublishUsername)
-//    password(jetbrainsPublishPassword)
-//    token(jetbrainsPublishToken)
-//}
 
 repositories {
     mavenCentral()
@@ -79,6 +62,7 @@ tasks {
         """)
         changeNotes("""
       <ul>
+        <li><b>1.1.2</b> <em>(2020-01-17)</em> - Compatible with Intellij IDEA 2020.1</li>
         <li><b>1.1.2</b> <em>(2020-01-17)</em> - ArrayOutOfBoundsException in Intellij IDEA 2019.3.2</li>
         <li><b>1.1.1</b> <em>(2019-05-30)</em> - Fix NoSuchMethodError on 2019.1</li>
         <li><b>1.1.0</b> <em>(2019-05-24)</em> - Create step definitions</li>
