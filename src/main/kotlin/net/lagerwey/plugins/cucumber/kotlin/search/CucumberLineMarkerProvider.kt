66bdec207d9c56ae4369c32fb934bf19af6615bc
Jos Lagerweij <jos@lagerwey.net>
2021-06-04 22:50:26 +0200

    Get rid of deprecated LineMarkerInfo constructor usages

--- src/main/kotlin/net/lagerwey/plugins/cucumber/kotlin/search/CucumberLineMarkerProvider.kt
+++ src/main/kotlin/net/lagerwey/plugins/cucumber/kotlin/search/CucumberLineMarkerProvider.kt
@@ -1,10 +1,13 @@
 package net.lagerwey.plugins.cucumber.kotlin.search
 
+import com.intellij.codeHighlighting.Pass
 import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.codeInsight.daemon.LineMarkerProvider
 import com.intellij.openapi.editor.markup.GutterIconRenderer
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.util.Function
 import icons.CucumberIcons
 import net.lagerwey.plugins.cucumber.kotlin.CucumberKotlinUtil
 import org.jetbrains.kotlin.psi.KtCallExpression
@@ -26,13 +29,15 @@ class CucumberLineMarkerProvider : LineMarkerProvider {
     }
 
     private fun createMarker(element: PsiElement, toolTip: String?): LineMarkerInfo<*> {
+        val anchor = PsiTreeUtil.getDeepestFirst(element)
         return LineMarkerInfo(
-                element,
-                element.textRange,
-                CucumberIcons.Cucumber,
-                { toolTip },
-                null,
-                GutterIconRenderer.Alignment.RIGHT
+            anchor,
+            anchor.textRange,
+            CucumberIcons.Cucumber,
+            { toolTip },
+            null,
+            GutterIconRenderer.Alignment.RIGHT,
+            { "" }
         )
     }
 }
